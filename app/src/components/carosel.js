253ea68c9f8b97c96d46d4e191d80979fbc9bc94
TheTrabin <beyondtears@gmail.com>
2020-09-12 02:59:55 -0700

    Worked on picture carosel

--- app/src/components/carosel.js
+++ app/src/components/carosel.js
@@ -3,11 +3,31 @@ import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
 const MyCarousel = () => (
-  <Carousel plugins={['arrows']}>
+    <div style={{ display: 'flex', justifyContent: 'center', flexFlow: 'row', alignContent: 'center', textAlign: 'center' }}>
+  <Carousel plugins={
+      ['infinite', 'arrows',
+      'fastSwipe',
+    {
+      resolve: slidesToShowPlugin,
+      options: {
+       numberOfSlides: 1
+      }
+    },
+      {
+        resolve: autoplayPlugin,
+        options: {
+          interval: 2000,
+        },
+      }]}
+       animationSpeed={1000}>
     <img src={imageOne} />
     <img src={imageTwo} />
     <img src={imageThree} />
   </Carousel>
+  <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </div>
+  </div>
 );
 
 export default MyCarousel;
